de06fc66948306e6760bbc572e8df28c77bfca99
Tony <tony@aosd.co.uk>
2024-10-18 15:56:37 +0100

    Checkpoint

--- src/main/java/uk/co/aosd/onto/biological/BiologicalEntity.java
+++ src/main/java/uk/co/aosd/onto/biological/BiologicalEntity.java
@@ -2,7 +2,9 @@ package uk.co.aosd.onto.biological;
 
 import java.util.Optional;
 
-public interface BiologicalEntity {
+import uk.co.aosd.onto.foundation.Individual;
+
+public interface BiologicalEntity extends Individual {
 
     Optional<DNA> dna();
 }
--- src/main/java/uk/co/aosd/onto/reference/HumanPerson.java
+++ src/main/java/uk/co/aosd/onto/reference/HumanPerson.java
@@ -3,10 +3,10 @@ package uk.co.aosd.onto.reference;
 import java.util.Optional;
 
 import uk.co.aosd.onto.biological.DNA;
+import uk.co.aosd.onto.biological.Human;
 import uk.co.aosd.onto.foundation.Class;
 import uk.co.aosd.onto.foundation.Event;
 import uk.co.aosd.onto.language.Language;
-import uk.co.aosd.onto.person.Human;
 import uk.co.aosd.onto.signifying.Signifying;
 
 public record HumanPerson(
