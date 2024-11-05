384146050a965c34a21d7cfdadcb25c4e425800d
Tony <tony@aosd.co.uk>
2024-11-05 20:25:49 +0000

    Code tidy

--- src/test/java/uk/co/aosd/onto/foundation/OrganisationTest.java
+++ src/test/java/uk/co/aosd/onto/foundation/OrganisationTest.java
@@ -13,8 +13,8 @@ import uk.co.aosd.onto.language.Language;
 import uk.co.aosd.onto.organisation.Membership;
 import uk.co.aosd.onto.organisation.Organisation;
 import uk.co.aosd.onto.reference.ClassImpl;
-import uk.co.aosd.onto.reference.LanguageImpl;
 import uk.co.aosd.onto.reference.HumanImpl;
+import uk.co.aosd.onto.reference.LanguageImpl;
 import uk.co.aosd.onto.reference.SignifierImpl;
 import uk.co.aosd.onto.signifying.Signifier;
 
@@ -31,16 +31,20 @@ public class OrganisationTest {
     public void testCreateOrganisations() {
 
         final Signifier<String> personSignifier = new SignifierImpl<>("1", "Alice Cooper", FROM, TO);
+        final Signifier<String> acmeSignifier = new SignifierImpl<>("2", "ACME Ltd", TO, FROM);
+
         final Class<Signifier<String>> person1Names = new ClassImpl<>("person1Names", Set.of(personSignifier));
+        final Class<Signifier<String>> orgNames = new ClassImpl<>("orgNames1", Set.of(acmeSignifier));
+
         final Language english = new LanguageImpl("en-GB", "British English");
         final Class<Language> languages = new ClassImpl<>("languages1", Set.of(english));
-        final Human person1 = new HumanImpl("Alice", TO, FROM, person1Names, english, languages, UNKNOWN_DNA);
-        final Membership ceo = new AcmeMembership("ceo", person1, FROM, TO);
-        final Set<Membership> memberships = Set.of(ceo);
+        final Human alice = new HumanImpl("Alice", TO, FROM, person1Names, english, languages, UNKNOWN_DNA);
+
+        final Membership ceoMembership = new AcmeMembership("ceo", alice, FROM, TO);
+        final Set<Membership> memberships = Set.of(ceoMembership);
         final Class<Membership> acmeTeamMemberships = new ClassImpl<>("acmeTopLevelTeam", memberships);
+
         final Class<Organisation> units = new ClassImpl<>("units1", Set.of());
-        final var acmeSignifier = new SignifierImpl<>("2", "ACME Ltd", TO, FROM);
-        final Class<Signifier<String>> orgNames = new ClassImpl<>("orgNames1", Set.of(acmeSignifier));
         final Org acme = new Org("org1", acmeTeamMemberships, "ACME makes widgets", units, orgNames, FROM, TO);
 
         assertNotNull(acme);
