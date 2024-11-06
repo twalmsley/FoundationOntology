b9e09a91e036b3df8584243f227758761b3d2f9b
Tony <tony@aosd.co.uk>
2024-11-06 13:39:12 +0000

    Code comments.

--- src/test/java/uk/co/aosd/onto/foundation/OrganisationTest.java
+++ src/test/java/uk/co/aosd/onto/foundation/OrganisationTest.java
@@ -32,30 +32,37 @@ public class OrganisationTest {
     @Test
     public void testCreateOrganisations() {
 
+        // Create some Signifiers for the person and the organisation.
         final Signifier<String> personSignifier1 = new SignifierImpl<>(randString(), "Alice Cooper", FROM, TO);
         final Signifier<String> personSignifier2 = new SignifierImpl<>(randString(), "Vincent Damon Furnier", FROM, TO);
         final Signifier<String> acmeSignifier1 = new SignifierImpl<>(randString(), "ACME Widgets Ltd", FROM, TO);
         final Signifier<String> acmeSignifier2 = new SignifierImpl<>(randString(), "ACME Ltd", FROM, TO);
 
+        // The signifiers need to be added to Classes (Sets)
         final Class<Signifier<String>> person1Names = new ClassImpl<>(randString(),
             Set.of(personSignifier1, personSignifier2));
         final Class<Signifier<String>> orgNames = new ClassImpl<>(randString(), Set.of(acmeSignifier1, acmeSignifier2));
 
+        // Create the languages that the person uses.
         final Language english = new LanguageImpl(randString(), "British English");
         final Language german = new LanguageImpl(randString(), "Deutsch");
         final Class<Language> languages = new ClassImpl<>(randString(), Set.of(english, german));
+
+        // Create the person
         final Human alice = new HumanImpl(randString(), FROM, TO, person1Names, english, languages, UNKNOWN_DNA);
 
+        // Create a Class of memberships for the person as a member of something.
         final Membership ceoMembership = new AcmeMembership(randString(), alice, FROM, TO);
-        final Set<Membership> memberships = Set.of(ceoMembership);
-        final Class<Membership> acmeTeamMemberships = new ClassImpl<>(randString(), memberships);
+        final Class<Membership> acmeTeamMemberships = new ClassImpl<>(randString(), Set.of(ceoMembership));
 
+        // Create an organisation with memberships and no sub-units.
         final Class<Organisation> units = new ClassImpl<>(randString(), Set.of());
-        final OrganisationImpl acme = new OrganisationImpl(randString(), acmeTeamMemberships, "ACME makes widgets", units, orgNames, FROM, TO);
+        final OrganisationImpl acme = new OrganisationImpl(randString(), acmeTeamMemberships, "ACME makes widgets",
+            units, orgNames, FROM, TO);
 
         assertNotNull(acme);
     }
-    
+
     private static String randString() {
         return UUID.randomUUID().toString();
     }
